76f97919e289afcca3fe229c1028bdc899e4baf1
Wolfgang Barth <wob@swobspace.net>
2014-04-13 10:49:02 +0200

    bugfix example unix.rb
      - connection.get needs type Livestatus::<Model>, not type String
      - Livestatus.Connection.new needs option hash, not a string

--- examples/unix.rb
+++ examples/unix.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 
+# for testing local stuff from examples directory
+# $LOAD_PATH.unshift '../lib'
+
 require 'livestatus'
 
-c = Livestatus::Connection.new("unix:///var/nagios/rw/live")
+c = Livestatus::Connection.new({:uri => "unix:///var/nagios/rw/live"})
 
 c.command("DISABLE_NOTIFICATIONS")
-puts c.get("status").inspect
+puts c.get(Livestatus::Status).inspect
