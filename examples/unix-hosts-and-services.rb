f69871cdcc2ae10400cdaaf9854dad9124cf734c
Wolfgang Barth <wob@swobspace.net>
2014-04-13 11:31:43 +0200

    example retrieving hosts and services using unix socket

---
+++ examples/unix-hosts-and-services.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# improved unix example with ideas from https://github.com/jedi4ever/livestatus
+
+# for testing local stuff from examples directory
+# $LOAD_PATH.unshift '../lib'
+
+require 'livestatus'
+require 'pp'
+
+connection = Livestatus::Connection.new({:uri => "unix:///var/nagios/rw/live"})
+
+connection.command("DISABLE_NOTIFICATIONS")
+
+tables = [Livestatus::Host, Livestatus::Service]
+
+tables.each do |table|
+  puts "### table #{table.to_s} ###"
+  rows = connection.get(table)
+  puts rows.to_yaml
+end
+
